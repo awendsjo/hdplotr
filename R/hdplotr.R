4ffb667c1bff9b95ffae3dedcaf3fb664a0a170e
awendsjo <awendsjo@gmail.com>
2020-09-21 23:09:51 +0200

    1

--- R/hdplotr.R
+++ R/hdplotr.R
@@ -1,8 +1,21 @@
-hdplotr <- function(width,height,units,resolution,x,y,mfrow,name,format){
-  if (format == "png"){
-    name <- paste0(name,".png")
-    png(name, width = width, height = height, units = units, res = resolution)
-    par(mfrow=mfrow)
-    plot(x=x,y=y)
-    dev.off()}
-  else print("Fan byt format (funkar bara med png")}
+hdplotr <- function(x,y,xlab="x",ylab="y",main=NULL,
+                    width=5,height=3.5,units="in",resolution=250,
+                    mfrow=c(1,1),name,format="png",save=FALSE,
+                    abline=NULL,legend=NULL){
+  if (save){
+    if (missing(name)) print("Error: What is the name of your file?")
+    else
+      if (format == "png"){
+        name <- paste0(name,".png")
+        plot.new()
+        png(name, width = width, height = height, units = units, res = resolution)
+        par(mfrow=mfrow)
+        plot(x=x,y=y,ylab=ylab,xlab=xlab,main=main)
+        abline(abline)
+        if (!missing(legend)) legend("topleft", legend=legend,cex=1)
+        dev.off()}
+    else print("I only work with png, sorry.")}
+  else plot(x=x,y=y,ylab=ylab,xlab=xlab,main=main)
+  abline(abline)
+  if (!missing(legend)) legend("topleft", legend=legend,cex=0.1)
+}
