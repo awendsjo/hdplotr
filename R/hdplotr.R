1f9720d0e5dfbafd72f52482cddf220292c3ad73
awendsjo <awendsjo@gmail.com>
2020-09-22 09:20:26 +0200

    1

--- R/hdplotr.R
+++ R/hdplotr.R
@@ -1,7 +1,17 @@
-hdplotr <- function(x,y,xlab="x",ylab="y",main=NULL,
-                    width=5,height=3.5,units="in",resolution=250,
-                    mfrow=c(1,1),name,format="png",save=FALSE,
-                    abline=NULL,legend=NULL){
+hdplotr <- function(x,
+                    y,
+                    xlab="x",
+                    ylab="y",
+                    main=NULL,
+                    width=5,
+                    height=3.5,
+                    units="in",
+                    resolution=250,
+                    mfrow=c(1,1),
+                    name,format="png",
+                    save=FALSE,
+                    abline=NULL,
+                    legend=NULL){
   if (save){
     if (missing(name)) print("Error: What is the name of your file?")
     else
@@ -15,7 +25,8 @@ hdplotr <- function(x,y,xlab="x",ylab="y",main=NULL,
         if (!missing(legend)) legend("topleft", legend=legend,cex=1)
         dev.off()}
     else print("I only work with png, sorry.")}
-  else plot(x=x,y=y,ylab=ylab,xlab=xlab,main=main)
+
+  else plotr()
   abline(abline)
   if (!missing(legend)) legend("topleft", legend=legend,cex=0.1)
 }
---
+++ R/plotr.R
@@ -0,0 +1,12 @@
+#' Plotr
+#'
+#' @param x
+#' @param y
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plotr  <- function(x,y) {
+  base::plot(x,y)
+}
