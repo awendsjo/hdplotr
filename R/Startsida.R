c0b786909406fa9072c1a4eafd8f6a02c4f83353
awendsjo <awendsjo@gmail.com>
2020-09-22 09:51:24 +0200

    1

--- R/Startsida.R
+++ R/Startsida.R
@@ -1,3 +1,1 @@
-library(devtools)
 load_all()
-check()
--- R/hdplotr.R
+++ R/hdplotr.R
@@ -10,19 +10,17 @@ hdplotr <- function(x,
                     mfrow=c(1,1),
                     name,
                     format="png",
-                    save=FALSE,
-                    abline=NULL,
-                    legend=NULL){
+                    save=FALSE,){
   if (save){
-    if (missing(name)) print("Error: What is the name of your file?")
+    if (missing(name)) stop("Error: What is the name of your file?")
     else
-      if (!format == "png") print("I only work with png, sorry.")
-      else
-        name <- paste0(name,".png")
-        plot.new()
-        png(name = name, width = width, height = height, units = units, res = resolution)
-        par(mfrow=mfrow)
-        plot(x=x,y=y,ylab=ylab,xlab=xlab,main=main)
-        dev.off()}
+      if (!format == "png") stop("I only work with png, sorry.")
+    else
+      name <- paste0(name,".png")
+    plot.new()
+    png(name, width = width, height = height, units = units, res = resolution)
+    par(mfrow=mfrow)
+    plot(x=x,y=y,ylab=ylab,xlab=xlab,main=main)
+    dev.off()}
   else plot(x=x,y=y,ylab=ylab,xlab=xlab,main=main)
 }
